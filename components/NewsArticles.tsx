d884d8a05a7aa90ee90316bf8d01b24a61018ce4
Exortile <xortile@gmail.com>
2023-12-29 17:03:22 -0500

    feature: added fetching and viewing news on homepage

--- components/NewsArticles.tsx
+++ components/NewsArticles.tsx
@@ -1,7 +1,21 @@
-export default function NewsArticles() {
+import NewsDisplay from "./NewsDisplay"
+
+export default async function NewsArticles() {
+  /*
+  hetkene probleem newsdata API'ga, tasuta plaaniga saab pmst max 10 artiklit ainult kätte
+  üle selle vist ei tundu et saab?
+  arvatavasti peab välja uurima tegelikud official API otspunktid ja neilt info kätte saama.
+  */
+  const newsdataApiKey = process.env.NEWSDATA_API_KEY!
+  const fNews = await fetch(
+    `https://newsdata.io/api/1/news?country=ee&apikey=${newsdataApiKey}`
+  )
+  const pNews = await fNews.json()
+
   return (
-    <div className="h-[80vh] w-[80vh] bg-slate-300 rounded-xl px-4 py-4">
+    <div className="h-screen w-[80vh] bg-slate-300 rounded-xl px-4 py-4">
       <p className="text-2xl text-black font-bold">Viimased uudised:</p>
+      <NewsDisplay news={pNews} />
     </div>
   )
 }
