34881df264951fd29f0073d7211b494f6869e4cc
Exortile <xortile@gmail.com>
2024-01-03 00:16:12 -0500

    Revert "refactor: moved other pages to page router"
    This reverts commit d9b31bdcaa6eb75f569794c2d62e6d65bb5460fc.

--- app/Majandus/page.tsx
+++ app/Majandus/page.tsx
@@ -1,17 +1,16 @@
 import HomeHeader from "@/components/HomeHeader"
 import NewsArticlesMajandus from "@/components/NewsArticlesMajandus"
 import NewsSideFilters from "@/components/NewsSideFilters"
-import MainLayout from "@/components/MainLayout"
 
 export default function Majandus() {
   return (
-    <MainLayout>
+    <main>
       <HomeHeader />
 
       <div className="flex flex-1 pt-8 w-[93%] mx-auto gap-10">
         <NewsSideFilters />
         <NewsArticlesMajandus />
       </div>
-    </MainLayout>
+    </main>
   )
 }
--- app/Sport/page.tsx
+++ app/Sport/page.tsx
@@ -1,17 +1,16 @@
 import HomeHeader from "@/components/HomeHeader"
 import NewsArticlesSport from "@/components/NewsArticlesSport"
 import NewsSideFilters from "@/components/NewsSideFilters"
-import MainLayout from "@/components/MainLayout"
 
 export default function Sport() {
   return (
-    <MainLayout>
+    <main>
       <HomeHeader />
 
       <div className="flex flex-1 pt-8 w-[93%] mx-auto gap-10">
         <NewsSideFilters />
         <NewsArticlesSport />
       </div>
-    </MainLayout>
+    </main>
   )
 }
