f6ed3fd2dbdfbd1f65c36c824c9f9b4ef2386a31
Exortile <xortile@gmail.com>
2024-01-04 01:32:06 -0500

    feat/refactor: added searchbar functionality, refactored the news pages to be client components (to support searching), changed nextjs config and made API key publicly accessible to browser

--- utils/newsdata/api.ts
+++ utils/newsdata/api.ts
@@ -1,8 +1,14 @@
-export const fetchNews = async (props: {
+"use client"
+
+import { useSearchParams } from "next/navigation"
+
+export type NewsFetchProps = {
   category?: string
   query?: string
   page?: string
-}) => {
+}
+
+export const fetchNews = async (props: NewsFetchProps) => {
   /*
   ühe API calliga saab max 10 artiklit kätte. need on "lehekülgedeks" jaotatud,
   seega lehekülgede vahetamiseks peab uue fetchi tegema.
@@ -12,14 +18,22 @@ export const fetchNews = async (props: {
   const queryParameter = props.query ? `&q=${props.query}` : ""
   const pageParameter = props.page ? `&page=${props.page}` : ""
 
-  const newsdataApiKey = process.env.NEWSDATA_API_KEY!
+  const newsdataApiKey = process.env.NEXT_PUBLIC_NEWSDATA_API_KEY!
   const fNews = await fetch(
     `https://newsdata.io/api/1/news?apikey=${newsdataApiKey}&language=et${queryParameter}${categoryParameter}${pageParameter}`
   )
 
   if (!fNews.ok) {
-    throw new Error("Fetching news failed")
+    throw new Error(
+      `Fetching news failed, status ${fNews.status} (${fNews.statusText})`
+    )
   }
 
   return fNews.json()
+}
+
+export const updateNewsProps = (props: NewsFetchProps) => {
+  const searchParams = useSearchParams()
+
+  if (searchParams.has("q")) props.query = searchParams.get("q") as string
 }
